25503d6906bc916992afc98e607632be81c7069c
SergioCastellanosL <SergioCastellanosL@github.com>
2020-10-30 17:24:05 -0500

    s

--- serialutility.c
+++ serialutility.c
@@ -1,7 +1,16 @@
 #include "serialutility.h"
 #include <avr/io.h>
+#include "colita.h"
 
-void Read_commands(Commands *com){
+void Inicializar_commands(Commands *com){
+    com->state= ESTADO0;
+    com->addcount= 0;
+    com->error= 0;
+    com->numcount= 0;
+    com->ok=0;
+}
+
+void Read_commands(Commands *com, Colita *p_cola){
     switch (com->state)
     {
         case ESTADO0:
@@ -85,8 +94,16 @@ void Read_commands(Commands *com){
                 com->addcount= 0;
                 break;
             }
-            com->add[com->addcount]= com->input;
-            com->addcount++;   
+            if(com->addount<4){
+                com->add[com->addcount]= com->input;
+                com->addcount++;  
+            }
+            else{
+                com->error= 1;
+                com->addcount=0;
+                com->state=ESTADO0;
+                break;
+            }  
         break;
     
         case ESTADO7: // 
@@ -104,11 +121,13 @@ void Read_commands(Commands *com){
             if((com->input)=='\n'){
                 //Realiza WB
                 com->state=ESTADO0;
+                com->ok=1;
                 com->numcount= 0;
                 break;
             }
-            com->val[com->numcount]= com->input;
-            com->numcount++;   
+            colocar_val_colita(&p_cola, com->input);
+            //com->val[com->numcount]= com->input;
+            //com->numcount++;   
         break;
     
         case ESTADO9: // 
@@ -117,13 +136,22 @@ void Read_commands(Commands *com){
                 com->numcount= 0;
                 break;
             }
-            com->num[com->numcount]= com->input;
-            com->numcount++;   
+            if(com->numcount<2){
+                com->num[com->numcount]= com->input;
+                com->numcount++;  
+            }
+            else{
+                com->error= 1;
+                com->numcount=0;
+                com->state=ESTADO0;
+                break;
+            } 
         break;
 
         case ESTADO10: // 
             if((com->input)=='\n'){
                 //Realiza WS
+                com->ok=1;
                 com->state=ESTADO0;
                 com->numcount= 0;
                 break;
@@ -131,8 +159,19 @@ void Read_commands(Commands *com){
             if((com->input)==','){
                 break;
             }
-            com->val[com->numcount]= com->input;
-            com->numcount++;   
+            if(!colita_full(&p_cola)){
+                colocar_val_colita(&p_cola,com->input);
+            }
+            else
+            {
+                /* Guardar los datos en memoria 
+                y mover las 10 posiciones de add*/
+                inicie_colita(&p_cola);
+                colocar_val_colita(&p_cola,com->input);
+            }
+            
+            //com->val[com->numcount]= com->input;
+            //com->numcount++;   
         break;
 
         default:
--- serialutility.h
+++ serialutility.h
@@ -26,22 +26,16 @@ struct Commands{
 	ESTADOS state;
 	char input;
 	char num[2];
-	char val[510];
+	//char val[510];
 	char add[4];
 	short numcount;
 	short addcount;
+	int error;
+	int ok;
 };
 
-void Read_commands(Commands *com); //Maquina de estados de la comunicacion serial
-void Su_inicie_uart(uint16_t ubrr, Comunicacion *com);	// Inicializa lo que se necesite
-														// Para comunicacion serial.
-void Su_Atencion_Bajo_Consumo(Comunicacion *com);		// Se configura el bajo consumo
-char Su_Hubo_Tecla_Serial(Comunicacion *tec_enable);	// Revisa si se ha recibido y si se
-														// recibio guarda el valor
-void Su_Transmision(int8_t *tempUnidades, int8_t *bandera, int8_t *tempDecenas);// Transmite
-														// unidades, decenas, centigrados y un espacio
-void Su_Interrupt_Enable(void);							// Habilita la interrupcion INT0
-void Su_Interrupt_Disable(void);						// Desabilita la interrupcion INT0
+void Inicializar_commands(Commands *com);
+void Read_commands(Commands *com, Colita *p_cola); //Maquina de estados de la comunicacion serial
 
 	#ifdef __cplusplus
 } // extern "C"
